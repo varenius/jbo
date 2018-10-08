27b2672fab4254e8ed2ff6b6a27b48b9161972f9
Eskil Varenius <eskil.varenius@gmail.com>
2018-10-08 15:12:38 +0000

    minimised client example

--- HSL2/hsl2client.py
+++ HSL2/hsl2client.py
@@ -3,21 +3,9 @@
 # NOTE: Simple client to request HSL2 dictionaries from a receiver server
 #       application which is parsing the binary multicast
 import json
-import socket, select
-import time
-import numpy as np
+import socket
 import errno
 import sys
-
-def print_teldata(td):
-    if td['status']:
-        arec = td['status']['receiverstatus']['currentrec']
-        act = np.array(td['status']['actual_azel'][0:2])*180/np.pi
-        dem = np.array(td['status']['demanded_azel'][0:2])*180/np.pi
-        toprint = td['status']['time_isot'], "Binary length", td['binary_message_length'], td['telname'], td['status']['control'], "Az ", act[0], " dem ", dem[0], " El ", act[1], " dem ", dem[1],"Receiver: ", arec
-    else:
-        toprint = td['telname'], td['telnumber'], 'NOSTATUS'
-    print(toprint)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
@@ -32,8 +20,8 @@ except socket.error as serr:
         sys.exit(1)
 
 # Define string to send to get data
-#msg = 'ALL'
-msg = 'Mark 2'
+#msg = 'ALL' # ALL for all telescopes, telname i.e. 'Mark 2' for Mk2 data etc.
+msg = 'Mark 2' # for single telescope, e.g. 'Mark 2'
 # If in python3, use bytestring
 if (sys.version_info > (3, 0)):
     msg = msg.encode()
@@ -45,10 +33,6 @@ while True:
     if (sys.version_info > (3, 0)):
         data = data.decode()
     teldata = json.loads(data)
-    #with open('data.json', 'w') as outfile:
-    #    json.dump(teldata, outfile, sort_keys=True, indent=4)
-    #print_teldata(teldata['Mark 2'])
     print(teldata)
     sys.exit()
-    time.sleep(1)
 s.close()
